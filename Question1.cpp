eb60b7e000bdf6dadacf5707340c883676276c27
CortoBeck <corto@DESKTOP-B68SND7.>
2024-12-30 11:47:59 +0100

    premiercommit

---
+++ Question1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <unordered_map>
+
+class graph
+{
+private:
+std::unordered_map<int, std::list<int>> adj;
+
+public:
+
+//Ajouter un sommet
+
+//Ajouter une arête 
+
+//Afficher la liste d'adjacence 
+
+
+}
